53605c88389ca0bbb55975c15afb6f35890def17
harshakanakala1 <harshakanakala1@gmail.com>
2023-10-26 22:16:33 +0530

    NGCONTENT

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NgcComponent } from './ngc/ngc.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NgcComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/ngc/ngc.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ngc',
+  templateUrl: './ngc.component.html',
+  styleUrls: ['./ngc.component.css']
+})
+export class NgcComponent {
+
+}
